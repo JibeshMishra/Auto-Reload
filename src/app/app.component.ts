4d9ba5df9944745af012feaf664acdb5aca10a82
Jibesh Mishra <jibeshmishra86@gmail.com>
2023-03-19 04:53:16 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'reload-root',
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'auto-reload';
+  counter=0;
+
+  LoadReload()
+  {
+    // debugger;
+    // window.location.reload();
+    // this.counter++;
+    console.log("Page not yet Reloaded !!");
+    if (window.localStorage) {
+      if (!localStorage.getItem('reload'))
+       {
+          localStorage['reload'] = true;
+          window.location.reload();
+       } else 
+       {
+          localStorage.removeItem('reload');
+       }
+  }
+    console.log("Page Reloaded !!");
+  }
+
+  LoadData()
+  {
+    debugger; 
+    console.log(this.counter);
+  }
+
+  ngOnInit(): void {
+    this.LoadReload();
+    this.LoadData();
+    //window.stop();
+  }
+  
+
 }
